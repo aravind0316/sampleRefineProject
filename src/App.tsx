76e85d02956c2ec20dab919ca147e884b98d77ba
Aravind Senthilkumar <aravind.senthilkumar@engro.io>
2023-01-10 11:22:40 +0530

    added refine

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Refine } from "@pankod/refine-core";
+import {
+    Layout,
+    ReadyPage,
+    notificationProvider,
+    ErrorComponent,
+} from "@pankod/refine-antd";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+
+import "@pankod/refine-antd/dist/reset.css";
+
+const App: React.FC = () => {
+    return (
+        <Refine
+            routerProvider={routerProvider}
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            Layout={Layout}
+            ReadyPage={ReadyPage}
+            notificationProvider={notificationProvider}
+            catchAll={<ErrorComponent />}
+        />
+    );
+};
+
+export default App;
